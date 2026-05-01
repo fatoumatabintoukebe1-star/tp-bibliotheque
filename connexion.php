14470af37a8b2a2855aa7a32c2713fb62b1fc139
fatoumatabintoukebe1-star <fatoumatabinetoukebe1@gmail.com>
2026-05-01 22:18:08 +0000

    connexion PDO

--- connexion.php
+++ connexion.php
@@ -1,2 +1,8 @@
-<?php
-echo "TP Bibliotheque";
+<?php 
+try{
+    $pdo = new PDO('mysql:host=localhost;dbname=tp-bibliotheque', 'root', '');
+    echo"Connexion réussie !";
+} catch (PDOException $e) {
+    echo"Erreur de connexion : " . $e->getMessage();
+}
+?>
