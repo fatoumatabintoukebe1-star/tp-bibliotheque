d27aea104f719fb9155156569a81d6a9e483141d
fatoumatabintoukebe1-star <fatoumatabinetoukebe1@gmail.com>
2026-05-02 17:20:49 +0000

    correction CRUD Categories

--- Categories.php
+++ Categories.php
@@ -2,32 +2,36 @@
 require_once 'connexion.php';
 
 class Categories {
+
 private $conn;
 private $table = "categories";
 
 public $id;
- public $libelle;
+public $libelle;
 
 public function __construct($db){
     $this->conn = $db;
 }
+
 public function create () {
-    $query = "INSERT TO ". $this->table . "(libelle) VALUES (:libelle)";
+    $query = "INSERT INTO " . $this->table . " (libelle) VALUES (:libelle)";
     $stmt = $this->conn->prepare($query);
-    $stmt->bindPram(":libelle", $this->libelle);
+    $stmt->bindParam(":libelle", $this->libelle);
     return $stmt->execute();
 }
+
 public function read () {
-    $query = "SELECT * FROM". $this->table . "ORDER BY libelle";
+    $query = "SELECT * FROM " . $this->table . " ORDER BY libelle";
     $stmt = $this->conn->prepare($query);
     $stmt->execute();
     return $stmt;
 }
+
 public function update () {
-    $query = "UPDATE". $this->table . "SET libelle = :libelle WHERE id = :id";
+    $query = "UPDATE " . $this->table . " SET libelle = :libelle WHERE id = :id";
     $stmt = $this->conn->prepare($query);
-    $stmt->bindPram(":libelle" , $this->libelle);
-    $stmt->bindPram(":id" . $this->id);
+    $stmt->bindParam(":libelle", $this->libelle);
+    $stmt->bindParam(":id", $this->id);
     return $stmt->execute();
 }
 public function delete() {
@@ -37,5 +41,4 @@ public function delete() {
     return $stmt->execute();
 }
 }
-
 ?>
