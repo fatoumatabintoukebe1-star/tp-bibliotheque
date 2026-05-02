bae35afca1ccd50e9cdd2944627868a2d9b63b55
fatoumatabintoukebe1-star <fatoumatabinetoukebe1@gmail.com>
2026-05-02 14:45:14 +0000

    Correction page connexion PDO

--- connexion.php
+++ connexion.php
@@ -1,8 +1,23 @@
 <?php 
-try{
-    $pdo = new PDO('mysql:host=localhost;dbname=tp-bibliotheque', 'root', '');
-    echo"Connexion réussie !";
-} catch (PDOException $e) {
-    echo"Erreur de connexion : " . $e->getMessage();
+ class Database {
+ private $host = "localhost";
+ private $db_name = "bibliotheque";
+ private $username = "root";
+ private $password = "";
+ private static $connexion = null;
+
+ public static function getConnexion() {
+if (self::$connexion === null) {
+try {
+ self::$connexion = new PDO("mysql:host=localhost;
+dbname=bibliotheque;charset=utf8", "root", "");
+ self::$connexion->setAttribute(PDO::ATTR_ERRMODE,
+PDO::ERRMODE_EXCEPTION);
+ } catch (PDOException $e) {
+ die("Erreur de connexion : " . $e->getMessage());
+ }
+ }
+ return self::$connexion;
+ }
 }
 ?>
